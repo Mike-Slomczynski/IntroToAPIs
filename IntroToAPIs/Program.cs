4361e27c3da93c84f695c0aff0e1f15d857c9a39
Michael Slomczynski <mike.slomczynski@gmail.com>
2021-09-13 22:39:00 -0500

    Rewrote Program.cs to match more conventional program style. Similar to .NET 5.

--- IntroToAPIs/Program.cs
+++ IntroToAPIs/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World!");
+namespace IntroToAPIs
+{
+    public static partial class APIKeys
+    {
+        public static readonly string GoogleIdentityAPIKey = "";
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
